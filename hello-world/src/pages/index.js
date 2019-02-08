2f8a6bb60e997b7f8fca29a6d45f43702b0d94fb
Brandon Mendez <bmednez0428@yahoo.com>
2019-02-08 01:47:35 -0600

    first commit section 0 - 3

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
-
-export default () => <div>Hello world!! This is Brandon's first site using Gatsby!</div>
+import {Link} from "gatsby"
+import Header from "../components/header"
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
